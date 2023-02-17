2fb618330d8da77e40966b7db9473223f58a5e1e
EbthalAbdelshafy <ebthalabdelshafy@gmail.com>
2023-02-17 13:48:54 -0800

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,9 +12,10 @@ int main(void)
 	int i;
 	char a;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = '0'; i <= '9'; i++)
+	{
 		putchar(i);
-
+	}
 	for (a = 'a'; a <= 'f'; a++)
 		putchar(a);
 	putchar('\n');
