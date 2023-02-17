d5e46277e68bc8e105cee522ce9b1e4bb3da7a7c
EbthalAbdelshafy <ebthalabdelshafy@gmail.com>
2023-02-17 12:22:37 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}
