5fcaa3be545a0752eca6c1537ab2b93e09bc370e
EbthalAbdelshafy <ebthalabdelshafy@gmail.com>
2023-02-17 14:27:11 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits.
+ * Return: Always 0 .
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9' ; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
