eae85e5b0ff6f803e53149002f06f62984e07ba9
EbthalAbdelshafy <ebthalabdelshafy@gmail.com>
2023-02-21 16:26:22 -0800

    task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include "main.h"
- /**
-  * print_alphabet -prints the alphabet
-  *
-  * Return: nothing, return void
-  */
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: nothing , return void
+ */
 
 void print_alphabet(void)
 {
