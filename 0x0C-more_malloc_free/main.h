66f5d654401273dff97359da70d97e9792155131
EbthalAbdelshafy <ebthalabdelshafy@gmail.com>
2023-03-16 20:59:22 -0700

    task 101

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int find_len(char *str);
+char *create_xarrays(int size);
+char *iterate_zeroes(char *str);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 
 #endif
