d5df32456b4a46f0cd84624d18d6682a89b90762
EbthalAbdelshafy <ebthalabdelshafy@gmail.com>
2023-03-20 18:35:14 -0700

    task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,9 +20,7 @@ struct dog
 };
 
 /**
- * dog_t - typedef for dog structure
- * type to define structure
- *
+ * dog_t - typedef for struct dog
  */
 
 typedef struct dog dog_t;
@@ -31,5 +29,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+void *_strcpy(char *dest, char *scr);
+int _strlen(char *s);
 
 #endif
